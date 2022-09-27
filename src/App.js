8403cf787a33734d3a1d91137dbe98389aa06d98
kparkhomenka <KParkhomenka@gomel.iba.by>
2022-09-27 17:57:20 +0300

    add-list-handler

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
+import { useState } from "react";
+
 import Expenses from "./components/Expenses/Expenses.js";
 import NewExpense from "./components/NewExpense/NewExpense.js";
 
+const initialExpenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(initialExpenses);
 
   const getNewExpense = (newExpense) => {
-    expenses.push({ id: "e" + (expenses.length + 1), ...newExpense });
+    setExpenses((prevExpenses) => [
+      { id: "e" + (expenses.length + 1), ...newExpense },
+      ...prevExpenses,
+    ]);
   };
 
   return (
     <div>
-      <h2>Let's get started!</h2>
       <NewExpense pushNewExpense={getNewExpense} />
       <Expenses items={expenses} />
     </div>
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,30 +7,28 @@ import ExpensesFilter from "./ExpensesFilter.js";
 import "./Expenses.css";
 
 function Expenses(props) {
-  const [selectedYearFilter, setSelectedYearFilter] = useState('2021');
+  const [selectedYearFilter, setSelectedYearFilter] = useState("2021");
 
   const filterChangeHandler = (changedDate) => {
     setSelectedYearFilter(changedDate);
   };
 
   return (
-    <Card className='expenses'>
-      <ExpensesFilter selected={selectedYearFilter} onChangeYearFilter={filterChangeHandler}/>
-      <ExpenseItem
-        date={props.items[0].date}
-        title={props.items[0].title}
-        amount={props.items[0].amount}
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={selectedYearFilter}
+        onChangeYearFilter={filterChangeHandler}
       />
-      <ExpenseItem
-        date={props.items[1].date}
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-      />
-      <ExpenseItem
-        date={props.items[2].date}
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-      />
+      {props.items
+        .filter((expense) => expense.date.getFullYear() == selectedYearFilter)
+        .map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
     </Card>
   );
 }
